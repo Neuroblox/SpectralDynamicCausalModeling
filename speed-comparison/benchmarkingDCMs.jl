06e176694c1d633e9bb95257408454457390f9e8
David Hofmann <david@lcneuro.org>
2025-05-16 10:22:37 +0200

    updated benchmarking procedure for LFPs

--- speed-comparison/benchmarkingDCMs.jl
+++ speed-comparison/benchmarkingDCMs.jl
@@ -7,6 +7,8 @@ using ForwardDiff
 using ComponentArrays
 using MetaGraphs
 using Graphs
+using Statistics
+using SparseArrays
 
 # both of the following are needed for typedefinitions even if no symbolic procedure is used
 using ModelingToolkit
@@ -18,9 +20,9 @@ using StatProfilerHTML
 
 include("../src/utils/typedefinitions.jl")
 include("../src/models/hemodynamic_response.jl")     # hemodynamic and BOLD signal model
-include("../src/variationallaplace/transferfunction.jl")
 include("../src/utils/helperfunctions.jl")
 include("../src/utils/helperfunctions_AD.jl")
+include("../src/variationallaplace/transferfunction.jl")
 include("../src/variationallaplace/optimization.jl")             # switch between _spm and _AD version.
 include("../src/utils/mar.jl")                       # multivariate auto-regressive model functions
 include("../src/utils/spDCMsetup.jl")
@@ -33,7 +35,7 @@ const t = ModelingToolkit.t_nounits
 const D = ModelingToolkit.D_nounits
 
 
-function wrapperfunction(vars)
+function wrapperfunction(vars; max_iter=128, dx=nothing)
     data = vars["data"];
     nr = size(data, 2);
     
@@ -58,12 +60,29 @@ function wrapperfunction(vars)
                   );
     
     csdsetup = (mar_order = 8, freq = vec(vars["Hz"]), dt = vars["dt"]);
-    
-    return setup_spDCM(data, initcond, csdsetup, priors, hyperpriors);
+    (state, setup) = setup_spDCM(data, initcond, csdsetup, priors, hyperpriors);
+
+    for iter in 1:max_iter
+        state.iter = iter
+        if dx === nothing
+            run_spDCM_iteration!(state, setup)
+        else
+            run_spDCM_iteration!(state, setup, dx)
+        end
+        if iter >= 4
+            criterion = state.dF[end-3:end] .< setup.tolerance
+            if all(criterion)
+                println("convergence after ", iter, " steps, with free energy: ", state.F[end])
+                break
+            end
+        end
+    end
+
+    return state
 end
 
 
-function wrapperfunction_MTK(vars)
+function wrapperfunction_MTK(vars; max_iter=128)
     data = vars["data"];
     nr = size(data, 2);
 
@@ -132,83 +151,216 @@ function wrapperfunction_MTK(vars)
 
     csdsetup = (mar_order = 8, freq = vec(vars["Hz"]), dt = vars["dt"]);
 
-    return setup_spDCM(data, fullmodel, initcond, csdsetup, priors, hyperpriors, indices, pmean, "fMRI");
+    (state, setup) =  setup_spDCM(data, fullmodel, initcond, csdsetup, priors, hyperpriors, indices, pmean, "fMRI");
+    for iter in 1:max_iter
+        state.iter = iter
+        run_spDCM_iteration!(state, setup)
+        if iter >= 4
+            criterion = state.dF[end-3:end] .< setup.tolerance
+            if all(criterion)
+                println("convergence after ", iter, " steps, with free energy: ", state.F[end])
+                break
+            end
+        end
+    end
+    return state
 end
 
-dx = exp(-8)
-max_iter = 128
+function wrapperfunction_MTK_lfp(vars; max_iter=128)
+    data = vars["csd"];
+    nr = size(data, 2);
+    dt = vars["dt"];
+    freqs = vec(vars["Hz"]);
+
+    ### Define priors and initial conditions ###
+    x = vars["x"];                       # initial condition of dynamic variabls
+
+    ########## assemble the model ##########
+    g = MetaDiGraph()
+    global_ns = :g # global namespace
+    regions = Dict()
+
+    @parameters lnr = 0.0
+    @parameters lnτ_ss=0 lnτ_sp=0 lnτ_ii=0 lnτ_dp=0
+    @parameters C=512.0 [tunable = false]    # TODO: SPM has this seemingly arbitrary 512 pre-factor in spm_fx_cmc.m. Can we understand why?
+    for ii = 1:nr
+        region = CanonicalMicroCircuitBlox(;namespace=global_ns, name=Symbol("r$(ii)₊cmc"), 
+                                            τ_ss=exp(lnτ_ss)*0.002, τ_sp=exp(lnτ_sp)*0.002, τ_ii=exp(lnτ_ii)*0.016, τ_dp=exp(lnτ_dp)*0.028, 
+                                            r_ss=exp(lnr)*2.0/3, r_sp=exp(lnr)*2.0/3, r_ii=exp(lnr)*2.0/3, r_dp=exp(lnr)*2.0/3)
+        add_blox!(g, region)
+        regions[ii] = nv(g)    # store index of neural mass model
+        taskinput = ExternalInput(;name=Symbol("r$(ii)₊ei"), I=1.0)
+        add_edge!(g, taskinput => region, weight = C)
+
+        # add lead field (LFP measurement)
+        measurement = LeadField(;name=Symbol("r$(ii)₊lf"))
+        # connect measurement with neuronal signal
+        add_edge!(g, region => measurement, weight = 1.0)
+    end
+
+    nl = Int((nr^2-nr)/2)   # number of links unidirectional
+    @parameters a_sp_ss[1:nl] = repeat([0.0], nl) # forward connection parameter sp -> ss: sim value 1/2
+    @parameters a_sp_dp[1:nl] = repeat([0.0], nl) # forward connection parameter sp -> dp: sim value 3/2
+    @parameters a_dp_sp[1:nl] = repeat([0.0], nl)  # backward connection parameter dp -> sp: sim value 1/16
+    @parameters a_dp_ii[1:nl] = repeat([0.0], nl) # backward connection parameters dp -> ii: sim value 3
+
+    k = 0
+    for i in 1:nr
+        for j in (i+1):nr
+            k += 1
+            # forward connection matrix
+            add_edge!(g, regions[i], regions[j], :weightmatrix,
+                    [0 exp(a_sp_ss[k]) 0 0;            # connection from sp to ss
+                    0 0 0 0;
+                    0 0 0 0;
+                    0 exp(a_sp_dp[k])/2 0 0] * 200)    # connection from sp to dp
+            # backward connection matrix
+            add_edge!(g, regions[j], regions[i], :weightmatrix,
+                    [0 0 0 0;
+                    0 0 0 -exp(a_dp_sp[k]);            # connection from dp to sp
+                    0 0 0 -exp(a_dp_ii[k])/2;          # connection from dp to ii
+                    0 0 0 0] * 200)
+        end
+    end
+
+    @named fullmodel = system_from_graph(g)
+
+    # attribute initial conditions to states
+    sts, idx_sts = get_dynamic_states(fullmodel)
+    idx_u = get_idx_tagged_vars(fullmodel, "ext_input")                # get index of external input state
+    idx_measurement, _ = get_eqidx_tagged_vars(fullmodel, "measurement")  # get index of equation of bold state
+    initcond = OrderedDict(sts .=> 0.0)
+    rnames = []
+    map(x->push!(rnames, split(string(x), "₊")[1]), sts);
+    rnames = unique(rnames);
+    for (i, r) in enumerate(rnames)
+        for (j, s) in enumerate(sts[r .== map(x -> x[1], split.(string.(sts), "₊"))])
+            initcond[s] = x[i, j]
+        end
+    end
+
+    modelparam = OrderedDict()
+    np = sum(tunable_parameters(fullmodel); init=0) do par
+        val = Symbolics.getdefaultval(par)
+        modelparam[par] = val
+        length(val)
+    end
+    indices = Dict(:dspars => collect(1:np))
+    # Noise parameter mean
+    modelparam[:lnα] = zeros(Float64, 2, nr);         # intrinsic fluctuations, ln(α) as in equation 2 of Friston et al. 2014 
+    n = length(modelparam[:lnα]);
+    indices[:lnα] = collect(np+1:np+n);
+    np += n;
+    modelparam[:lnβ] = [-16.0, -16.0];                # global observation noise, ln(β) as above
+    n = length(modelparam[:lnβ]);
+    indices[:lnβ] = collect(np+1:np+n);
+    np += n;
+    modelparam[:lnγ] = (-16.0)*ones(Float64, 2, nr);  # region specific observation noise
+    n = length(modelparam[:lnγ]);
+    indices[:lnγ] = collect(np+1:np+n);
+    np += n
+    indices[:u] = idx_u
+    indices[:m] = idx_measurement
+    indices[:sts] = idx_sts
+
+    # define prior variances
+    paramvariance = copy(modelparam)
+    paramvariance[:lnα] = ones(Float64, size(modelparam[:lnα]))./128.0; 
+    paramvariance[:lnβ] = ones(Float64, 2)./128.0;
+    paramvariance[:lnγ] = ones(Float64, size(modelparam[:lnγ]))./128.0;
+    for (k, v) in paramvariance
+        if occursin("a_", string(k))
+            paramvariance[k] = 1/16.0
+        elseif "lnr" == string(k)
+            paramvariance[k] = 1/64.0;
+        elseif occursin("lnτ", string(k))
+            paramvariance[k] = 1/32.0;
+        elseif occursin("lf₊L", string(k))
+            paramvariance[k] = 64;
+        end
+    end
+
+    priors = (μθ_pr = modelparam,
+            Σθ_pr = diagm(vecparam(paramvariance))
+    );
+
+    csdsetup = (mar_order = 8, freq = vec(vars["Hz"]), dt = vars["dt"]);
+    nf = length(csdsetup.freq)
+    Q = [spzeros(nf*nr^2, nf*nr^2) for i = 1:nr^2];
+    for (i, q) in enumerate(Q)
+        q[diagind(q)[((i-1)*nf+1):(nf*i)]] .= 1.0
+    end
+    
+    hyperpriors = (
+                    Q = Q,      # prior metaparameter precision, needs to be a matrix
+                  );
+
+    (state, setup) = setup_spDCM(data, fullmodel, initcond, csdsetup, priors, hyperpriors, indices, modelparam, "LFP");
+    for iter in 1:max_iter
+        state.iter = iter
+        run_spDCM_iteration!(state, setup)
+        print("iteration: ", iter, " - F:", state.F[end] - state.F[2], " - dF predicted:", state.dF[end], "\n")
+        if iter >= 4
+            criterion = state.dF[end-3:end] .< setup.tolerance
+            if all(criterion)
+                print("convergence\n")
+                break
+            end
+        end
+    end
+    return state
+end
+
+BLAS.set_num_threads(1)
+BLAS.get_num_threads()
+
 # speed comparison between different DCM implementations
-for n in 2:10
-    vals = matread("speed-comparison/matlab_" * string(n) * "regions.mat");
+for n in 2:6
+    vals = matread("speed-comparison/fastspeed/matlab_" * string(n) * "regions.mat");
 
-    (state, setup) = wrapperfunction(vals)
-    run_spDCM_iteration!(state, setup, dx)
-    (state, setup) = wrapperfunction(vals)
-    t_juliaSPM = @elapsed for iter in 1:max_iter
-        state.iter = iter
-        run_spDCM_iteration!(state, setup, dx)
-        if iter >= 4
-            criterion = state.dF[end-3:end] .< setup.tolerance
-            if all(criterion)
-                println("convergence after ", iter, " steps, with free energy: ", state.F[end])
-                break
-            end
-        end
-    end
-    F_SPM = state.F[end]
-    iter_SPM = state.iter
+    wrapperfunction(vals, max_iter=1, dx=exp(-8))
+    t_juliaSPM = @elapsed spm_state = wrapperfunction(vals, dx=exp(-8))
 
-    (state, setup) = wrapperfunction(vals)
-    run_spDCM_iteration!(state, setup)
-    (state, setup) = wrapperfunction(vals)
-    t_juliaAD = @elapsed for iter in 1:max_iter
-        state.iter = iter
-        run_spDCM_iteration!(state, setup)
-        if iter >= 4
-            criterion = state.dF[end-3:end] .< setup.tolerance
-            if all(criterion)
-                println("convergence after ", iter, " steps, with free energy: ", state.F[end])
-                break
-            end
-        end
-    end
-    F_AD = state.F[end]
-    iter_AD = state.iter
+    wrapperfunction(vals, max_iter=1)
+    t_juliaAD = @elapsed ad_state = wrapperfunction(vals)
 
-    (state, setup) = wrapperfunction_MTK(vals)
-    run_spDCM_iteration!(state, setup)
-    (state, setup) = wrapperfunction_MTK(vals)
-    t_juliaMTK = @elapsed for iter in 1:max_iter
-        state.iter = iter
-        run_spDCM_iteration!(state, setup)
-        if iter >= 4
-            criterion = state.dF[end-3:end] .< setup.tolerance
-            if all(criterion)
-                println("convergence after ", iter, " steps, with free energy: ", state.F[end])
-                break
-            end
-        end
-    end
-    F_MTK = state.F[end]
-    iter_MTK = state.iter
+    wrapperfunction_MTK(vals, max_iter=1)
+    t_juliaMTK = @elapsed mtk_state = wrapperfunction_MTK(vals)
     @show "Iteration:", n, t_juliaAD, t_juliaSPM, t_juliaMTK
 
     matwrite("speedcomp" * string(n) * "regions.mat", Dict(
         "t_mat" => vals["matcomptime"],
         "F_mat" => vals["F"],
         "t_jad" => t_juliaAD,
-        "F_jad" => F_AD,
+        "F_jad" => ad_state.F[end],
         "t_jspm" => t_juliaSPM,
-        "F_jspm" => F_SPM,
+        "F_jspm" => spm_state.F[end],
         "t_mtk" => t_juliaMTK,
-        "F_mtk" => F_MTK,
-        "iter_spm" => iter_SPM,
-        "iter_ad" => iter_AD,
-        "iter_mtk" => iter_MTK
+        "F_mtk" => mtk_state.F[end],
+        "iter_spm" => spm_state.iter,
+        "iter_ad" => ad_state.iter,
+        "iter_mtk" => mtk_state.iter
     ); compress = true)    
 end
 
-error()
+
+
+for n in 2:5
+    vals = matread("speed-comparison/cmc_" * string(n) * "regions.mat");
+
+    wrapperfunction_MTK_lfp(vals, max_iter=1)
+    t_julialfp = @elapsed mtk_state = wrapperfunction_MTK_lfp(vals)
+    @show "Iteration:", n, t_julialfp
+
+    matwrite("speed-comparison/speedcomp_lfp_improved_sparse_" * string(n) * "regions.mat", Dict(
+        "t_mat" => vals["matcomptime"],
+        "F_mat" => vals["F"],
+        "t_mtk" => t_julialfp,
+        "F_mtk" => mtk_state.F[end],
+        "iter_mtk" => mtk_state.iter
+    ); compress = true)    
+end
+
 
 ### Profiling ###
 # include("../src/VariationalBayes_AD.jl")
