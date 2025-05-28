dcbffe1f453b1c2ab8a034b805cad94f1bd36285
David Hofmann <david@lcneuro.org>
2025-05-28 09:28:15 +0200

    WIP: creating tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,8 @@
-using SafeTestsets
+using SafeTestsets, Neuroblox
+# using Test, TestSetExtensions, SafeTestsets, Logging
+
+Logging.global_logger(NullLogger())
+
+@testset ExtendedTestSet "NeurobloxGUI tests" begin
+  @includetests ARGS
+end
